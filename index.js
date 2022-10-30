6e9b5965dd29e899032ad8d1a3f7a8b68ee8fbd0
arielschinocca <96122546+arielschinocca@users.noreply.github.com>
2022-10-30 19:56:32 -0300

    se agrego carrusel nuevo

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const punto = document.querySelectorAll('.punto');
 //recorrer todos los puntos
 punto.forEach ((cadaPunto , i )=> {
     //asignamos un Click a cada punto
-    punto[i].addEventListener('click',()=>{
+    punto[i].addEventListener('click',()=> {
         //guardar posicion de cada punto
         let posicion = i
         //calcular el espacio que debe desplazase el grande
@@ -18,10 +18,10 @@ punto.forEach ((cadaPunto , i )=> {
 
 
         //movemos el grande
-        grande.style.transform = `translateX(${ operacion}%)`
+        grande.style.transform = `translateX(${ operacion }%)`
 
         //recorremos todos los punto
-        punto.forEach( (cadaPunto , i)=>{
+        punto.forEach( (cadaPunto , i)=> {
             //quitamos la clase activo a todos los punto
             punto[i].classList.remove('activo')
         })
@@ -32,3 +32,35 @@ punto.forEach ((cadaPunto , i )=> {
 
     })
 })
+
+
+const carruselAutomatico = document.querySelector(".carrusel-items");
+
+let intervalo = null;
+let step = 1;
+let maxScrollleft = carruselAutomatico.scrollWidth - carruselAutomatico.clientWidth;
+
+const start = () => {
+    intervalo = setInterval(function () {
+        carruselAutomatico.scrollLeft = carruselAutomatico.scrollLeft + step;
+        if (carruselAutomatico.scrollLeft === maxScrollleft) {
+            step = step * -1;
+        } else if (carruselAutomatico.scrollLeft === 0) {
+            step = step * -1;
+        }
+    }, 10);
+};
+
+const stop = () => {
+    clearInterval(intervalo);
+};
+
+carruselAutomatico.addEventListener('mouseover', ()=> {
+    stop();
+});
+
+carruselAutomatico.addEventListener('mouseover', ()=> {
+    start();
+});
+
+start();

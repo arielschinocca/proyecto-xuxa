fae54e4fb5b027c0455010f80f1740623a915b5c
arielschinocca <96122546+arielschinocca@users.noreply.github.com>
2022-11-07 20:49:09 -0300

    se modifico php4

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
-<?php
-
-include_once('db.php');
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href= "mensajeForm.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Gracias por tu mensaje, te respondemos a la brevedad</h1>
+    <?php
 
 
 //Conecto mi form / nombre, correo, telefono y mensaje/ con el server a traves del name.
@@ -10,16 +18,15 @@ $nombre = $_POST['nombre'];
 $correo = $_POST['correo'];
 $telefono = $_POST['telefono'];
 $mensajeForm = $_POST['mensaje'];
-$enviar = $_POST['submit'];
 
-
+/*
 $conectar=conn(); //ejecuta las conexiones a la base de datos
 $sql="INSERT INTO usuarios ( nombre, correo, telefono, mensaje) 
 VALUES ('$nombre', '$correo', '$telefono', '$mensajeForm' )";
 $resul = mysqli_query($conectar , $sql)or trigger_error("Query Failed! SQL- Error: ".mysqli_error($conectar));
 
 echo "$sql";
-
+*/
 
 // como me va a llegar el cuerpo del mail a mi, osea lo que la gente escribio en el formulario
 
@@ -37,7 +44,18 @@ mail($para, $asunto, utf8_decode($mensaje), $header);
 
 //Redireccion al haber enviado el form
 
-header('Location: http://127.0.0.1:5500/proyecto-xuxa/mensajeForm.html');
+header('Location:index.html');
 
 
 ?>
+    <script>
+        setTimeout(function(){
+            window.location.replace('index.html');
+        },2000);
+    </script>
+
+</body>
+</html>
+
+
+

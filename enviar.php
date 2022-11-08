768c046bb8bc8b85e602939112dfcf064d948854
arielschinocca <96122546+arielschinocca@users.noreply.github.com>
2022-11-08 16:18:43 -0300

    se modifico enviar.php

--- enviar.php
+++ enviar.php
@@ -1,63 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href= "mensajeForm.css">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Gracias por tu mensaje, te respondemos a la brevedad</h1>
-    <?php
+<?php
+
+if ( isset($_POST['nombre'])) {
+    if ( !empty($_POST['nombre']) && !empty($_POST['correo'] && !empty($_POST['mensaje']))) {
+        $name = $_POST['nombre'];
+        $email = $_POST['correo'];
+        $telefono = $_POST['telefono'];
+        $mensaje = $_POST['mensaje'];
 
 
-//Conecto mi form / nombre, correo, telefono y mensaje/ con el server a traves del name.
+        $para = "From: ariel.schinocca@live.com.ar" . "\r\n";
+        $header = $_POST['correo'] . "\r\n";
+        $header .= $_POST['nombre']. "\r\n";
+        $header .= $_POST['telefono'];
+        $header = "X-Mailer: PHP/" . phpversion();
+        $header =  $_POST['correo'];
 
-$nombre = $_POST['nombre'];
-$correo = $_POST['correo'];
-$telefono = $_POST['telefono'];
-$mensajeForm = $_POST['mensaje'];
+        $mail = mail($para, $name, $mensaje, $header );
+        if ($mensaje) {
+            echo "<h4> mail enviado exitosamente! </h4>";
+        }
+    }
+}
 
-$header  = 'From: ' . $correo . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+// $nombre = $_POST['nombre'];
+// $mail = $_POST['email'];
+// $mensaje = $_POST['mensaje'];
 
-/*
-$conectar=conn(); //ejecuta las conexiones a la base de datos
-$sql="INSERT INTO usuarios ( nombre, correo, telefono, mensaje) 
-VALUES ('$nombre', '$correo', '$telefono', '$mensajeForm' )";
-$resul = mysqli_query($conectar , $sql)or trigger_error("Query Failed! SQL- Error: ".mysqli_error($conectar));
+// $header = 'From: ' . $mail . " \r\n";
+// $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+// $header .= "Mime-Version: 1.0 \r\n";
+// $header .= "Content-Type: text/plain";
 
-echo "$sql";
-*/
+// $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+// $mensaje .= "Su e-mail es: " . $mail . " \r\n";
+// $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+// $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-// como me va a llegar el cuerpo del mail a mi, osea lo que la gente escribio en el formulario
+// $para = 'fernandocibau@hotmail.com';
+// $asunto = 'Mensaje de mi sitio web';
 
-$mensaje  = "Este mensaje fue enviado por" . $nomnbre . ",\r\n";
-$mensaje .= "Su e-mail es: " . $correo .  ",\r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] .  ",\r\n";
-$mensaje .= "Enviado el " . date('d/m/Y' , time());
+// mail($para, $asunto, utf8_decode($mensaje), $header);
 
-$para = 'ariel.schinocca@live.com.ar'; // el mail a donde van a llegar los mensajes
-$asunto = 'Mensaje de mi sitio web'; // el asunto de los mails que me llegan
-
-//funcion mail
-
-mail($para, $asunto, utf8_decode($mensaje), $header);
-
-//Redireccion al haber enviado el form
-
-header('Location:index.html');
-
-
+// header("Location:index.html");
 ?>
-    <script>
-        setTimeout(function(){
-            window.location.replace('index.html');
-        },2000);
-    </script>
-
-</body>
-</html>
